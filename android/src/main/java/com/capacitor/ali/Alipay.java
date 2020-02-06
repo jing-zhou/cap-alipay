d828c757e56833a0b965543016037fb1768a9f83
jingzhou <jingzhou_wang@hotmail.com>
2020-02-06 14:08:56 +0800

    Alipay.java initial implement

--- android/src/main/java/com/capacitor/ali/Alipay.java
+++ android/src/main/java/com/capacitor/ali/Alipay.java
@@ -5,16 +5,53 @@ import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.alipay.sdk.app.PayTask;
+
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
 
 @NativePlugin()
 public class Alipay extends Plugin {
+    private static final String bill = "bill";
+    private static final String status = "resultStatus";
+    private static final String memo = "memo";
+    private static final String success = "9000";
+    private static final String nobill = "Must provide the bill string";
 
     @PluginMethod()
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void pay(PluginCall call) {
+        if (!call.getData().has(bill)) {
+            call.reject(nobill);
+            return;
+        }
+        final String bstr = call.getString(bill);
 
-        JSObject ret = new JSObject();
-        ret.put("value", value);
-        call.success(ret);
+        CompletableFuture<Map<String, String>> future = CompletableFuture.supplyAsync(new Supplier<Map<String, String>>() {
+            @Override
+            public Map<String, String> get() {
+                return (new PayTask(getActivity()).payV2(bstr, true));
+            }
+        });
+
+        try {
+            Map<String, String> res = future.get();
+            String state = res.get(status);
+            if (success.equals(state)) {
+                JSObject ret = new JSObject();
+                for (Map.Entry<String, String> e : res.entrySet()) {
+                    ret.put(e.getKey(), e.getValue());
+                }
+                call.resolve(ret);
+            }
+
+            call.reject(res.get(memo));
+        } catch (ExecutionException | InterruptedException e) {
+            call.reject(e.getMessage());
+
+        }
+
     }
+
 }
