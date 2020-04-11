3b60e06b8e4b4521c3c1176194a0debcbc56f59a
jingzhou <jingzhou_wang@hotmail.com>
2020-04-11 12:46:52 +0800

    Alipay.pay(), handleMessage(): session safe

--- android/src/main/java/com/capacitor/ali/Alipay.java
+++ android/src/main/java/com/capacitor/ali/Alipay.java
@@ -3,14 +3,15 @@ package com.capacitor.ali;
 import android.os.Handler;
 import android.os.Message;
 
+import com.alipay.sdk.app.PayTask;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.alipay.sdk.app.PayTask;
 
 import java.lang.ref.WeakReference;
+import java.util.HashMap;
 import java.util.Map;
 
 @SuppressWarnings("unused")
@@ -18,12 +19,43 @@ import java.util.Map;
 public class Alipay extends Plugin {
     private static final int apay = 101;
     private static final String bill = "bill";
+    private static final String result = "res";
+    private static final String callObj = "call";
     private static final String status = "resultStatus";
     private static final String success = "9000";
     private static final String nobill = "Must provide the bill string";
 
-    // not a perfect solution
-    private PluginCall myCall;
+    private  MyHandler _handler;
+
+    @SuppressWarnings("unused")
+    @PluginMethod()
+    public void pay(final PluginCall call) {
+        if (!call.getData().has(bill)) {
+            call.reject(nobill);
+            return;
+        }
+
+        final String bstr = call.getString(bill);
+
+        new Thread(() -> {
+            Message msg = new Message();
+            msg.what = apay;
+            Map<String, Object> map = new HashMap<>();
+            map.put(callObj, call);
+            map.put(result, new PayTask(getActivity()).payV2(bstr, true));
+            msg.obj = map;
+            hanlder().sendMessage(msg);
+
+        }).start();
+
+    }
+
+    private MyHandler hanlder() {
+        if (_handler == null) {
+            _handler = new MyHandler(this);
+        }
+        return _handler;
+    }
 
     // https://stackoverflow.com/questions/11407943/this-handler-class-should-be-static-or-leaks-might-occur-incominghandler
     //static inner class doesn't hold an implicit reference to the outer class
@@ -39,55 +71,29 @@ public class Alipay extends Plugin {
         public void handleMessage(Message msg) {
             Alipay alipay = alipayWeakReferencee.get();
             if (alipay != null) {
-                PluginCall ca = alipay.myCall;
                 if (apay == msg.what) {
-                    if (ca == null) {
+                    Map<String, Object> map = ((Map) msg.obj);
+                    PluginCall call = (PluginCall) map.get(callObj);
+                    if (call == null) {
                         return;
                     }
                     @SuppressWarnings("unchecked")
-                    Map<String, String> res = (Map<String, String>) msg.obj;
+                    Map<String, String> res = (Map<String, String>) map.get(result);
                     String state = res.get(status);
                     if (success.equals(state)) {
                         JSObject ret = new JSObject();
                         for (Map.Entry<String, String> e : res.entrySet()) {
                             ret.put(e.getKey(), e.getValue());
                         }
-                        ca.resolve(ret);
+                        call.resolve(ret);
                         return;
                     }
 
-                    ca.reject(state);
+                    call.reject(state);
 
                 }
             }
         }
-    }
-
-    private final MyHandler mHandler = new MyHandler(this);
-
-    @SuppressWarnings("unused")
-    @PluginMethod()
-    public void pay(PluginCall call) {
-        if (!call.getData().has(bill)) {
-            call.reject(nobill);
-            return;
-        }
-
-        final String bstr = call.getString(bill);
-        myCall = call;
-
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-
-                Message msg = new Message();
-                msg.what = apay;
-                msg.obj = new PayTask(getActivity()).payV2(bstr, true);
-                mHandler.sendMessage(msg);
-            }
-        }).start();
-
     }
 
 }
