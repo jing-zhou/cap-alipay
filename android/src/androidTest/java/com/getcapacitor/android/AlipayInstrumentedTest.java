2cd7646c7f23d0dfb628362680aa0d92df70831c
jingzhou <jingzhou_wang@hotmail.com>
2024-10-16 11:28:00 +0800

    AlipayInstrumentedTest: debug

--- android/src/androidTest/java/com/getcapacitor/android/AlipayInstrumentedTest.java
+++ android/src/androidTest/java/com/getcapacitor/android/AlipayInstrumentedTest.java
@@ -3,10 +3,8 @@ package com.getcapacitor.android;
 import android.content.Context;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;
 
 /**
